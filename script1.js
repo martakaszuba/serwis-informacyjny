6fb414338580ec834807c25e042240fff148c75b
Marta Kaszuba <48859858+martakaszuba@users.noreply.github.com>
2019-05-18 13:03:14 +0200

    Add files via upload

---
+++ script1.js
@@ -0,0 +1,24 @@
+document.addEventListener("DOMContentLoaded", Load);
+function Load(){
+    fetch("https://newsapi.org/v2/top-headlines?country=pl&category=business&apiKey=5611edf8385a4cce97e57f8beec3264f")
+   .then(function(response){
+       return response.json();
+   })
+   .then(function(data){
+       var articles = data.articles;
+       var main = document.querySelector("#main");
+       articles.forEach(function(val){
+        main.innerHTML += `<a href=${val.url}
+        <div class="product shadow-lg">
+            <div class="title">
+                <h4>${val.source.name}</h4>
+                </div>
+                <div class="content">
+            <p>${val.title}</p>
+            </div>
+            </div>
+            </a>`
+       })
+
+   });
+}
